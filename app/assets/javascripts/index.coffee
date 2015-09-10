23760c98d5a2da7c13888b17937c3e014dd17ed5
vasa47 <vasudhara.v@gmail.com>
2015-09-10 22:43:52 +0200

    + delete , modify, get single car results done.

--- app/assets/javascripts/index.coffee
+++ app/assets/javascripts/index.coffee
@@ -1,6 +1,7 @@
 $ ->
-  getBars()
-  $("#addBar").submit (event) ->
+    getBars()
+   $("#addBar").submit (event) ->
+    alert("after there")
     event.preventDefault()
     $.ajax
       url: event.target.action
@@ -8,19 +9,36 @@ $ ->
       contentType: "application/json"
       data: JSON.stringify({
         name: $("#carName").val(),
-        fuel:$("#fuelType").val()
-        price:$("#price").val()
+        fuel:$("#fuelType").val(),
+        price:$("#price").val(),
         condition:$("#condition").val(),
         mileage:$("#mileage").val(),
         firstreg:$("#firstReg").val()
       })
       success: () ->
         getBars()
-        $("#barName").val("")
+        $("#carName").val("")
 
-  
+
+    $("#editCar").submit (event) ->
+      alert("we ll get here")
+      event.preventDefault()
+      $.ajax
+        url: event.target.action
+        type: event.target.method
+        contentType: "application/json"
+        data: JSON.stringify({
+
+        })
+        success: () ->
+          getBars()
+          $("#carName").val("")
+
 getBars = () ->
   $.get "/bars", (bars) ->
     $("#bars").empty()
     $.each bars, (index, bar) ->
-      $("#bars").append $("<li>").text bar.fuel
+      $("#bars").append $("<li>").text bar.name
+
+
+
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import com.sun.corba.se.impl.orbutil.threadpool.TimeoutException
 import play.api.mvc.{Action, Controller}
 import org.apache.hadoop.hbase.{HColumnDescriptor, HTableDescriptor, HBaseConfiguration}
 import org.apache.hadoop.hbase.client._
@@ -8,6 +9,7 @@ import play.api.Logger
 import play.api.libs.json.Json
 import java.util.UUID
 import scala.collection.JavaConversions._
+import scala.sys.process.processInternal.IOException
 
 object Application extends Controller {
 
@@ -35,7 +37,9 @@ object Application extends Controller {
     // return the server-side generated webpage from app/views/index.scala.html
     Ok(views.html.index("Play Framework + Scala + Hbase"))
   }
-  
+
+  // Function to add Cars
+
   def addBar() = Action(parse.json) { request =>
     // create a new row in the table that contains the JSON sent from the client
     val table = new HTable(hbaseConfig, barsTableName)
@@ -45,7 +49,8 @@ object Application extends Controller {
     table.close()
     Ok
   }
-  
+
+  // function to get View of Cars.
   def getBars = Action {
     // query the table and return a JSON list of the bars in the table
     val table = new HTable(hbaseConfig, barsTableName)
@@ -63,4 +68,73 @@ object Application extends Controller {
     Ok(Json.toJson(results))
   }
 
+ // Function to Modify Car if exist by id.
+  def modifyCar() = Action(parse.json) {  request =>
+    val table = new HTable(hbaseConfig, barsTableName)
+    val rowId = request.getQueryString("carId").toString
+    // Just check for the existence of the Car and Report Error if not there
+    val carData = new Get(Bytes.toBytes(rowId))
+    val carModifydata = table.get(carData)
+    try {
+      if(!(carModifydata == null || carModifydata.isEmpty())) {
+
+        val put = new Put(Bytes.toBytes(request.getQueryString("carId").toString))
+        put.add(family, qualifier, Bytes.toBytes(request.body.toString()))
+        table.put(put)
+        table.close()
+
+      }else {
+        // return error log error
+        println("Expection happens")
+      }
+
+    }catch {
+      case e: NullPointerException => println(e.printStackTrace())
+    }
+
+    Ok()
+  }
+
+  // function to delete Car by ID
+    def deleteCar() = Action(parse.json) { request =>
+    val table = new HTable(hbaseConfig, barsTableName)
+    val rowId = request.getQueryString("carId").toString
+    // Just check for the existence of the Car and Report Error if not there
+    val carData = new Get(Bytes.toBytes(rowId))
+    val carModifydata = table.get(carData)
+
+    try {
+      if(!(carModifydata == null || carModifydata.isEmpty())){
+        val deleteCar = new Delete(Bytes.toBytes(rowId))
+        table.delete(deleteCar)
+        table.close()
+      }
+    }catch{
+      case e: NullPointerException => print(e.printStackTrace())
+      case e: IOException => print(e.printStackTrace())
+    }
+    Ok
+    }
+
+
+  // function to get a car by ID.
+  def getOneCar() = Action(parse.json) { request =>
+
+    val table = new HTable(hbaseConfig, barsTableName)
+    val rowId = request.getQueryString("carId").toString
+    // Just check for the existence of the Car and Report Error if not there
+    val carData = new Get(Bytes.toBytes(rowId))
+    val carModifydata = table.get(carData)
+    if((carModifydata == null || carModifydata.isEmpty())) {
+      Ok("Not found")
+
+    }
+    val getCarData = new Get(Bytes.toBytes(rowId))
+    val carDataList = table.get(getCarData)
+    val results = carDataList.getColumn(family, qualifier)
+    table.close()
+
+
+    Ok(Json.toJson(results))
+  }
 }
